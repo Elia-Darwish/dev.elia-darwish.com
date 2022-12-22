c1690d062e11e5c0cd2f3c00169474476babf514
Elia Darwish <37368987+Elia-Darwish@users.noreply.github.com>
2022-12-22 22:03:18 +0100

    Revert "remove ssr"
    This reverts commit 218b82f31998677e804648ad81cd209a52e44494.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 import { darkTheme, getCssText } from 'stitches.config'
 import { KEY, Themes } from 'state/theme'
@@ -11,16 +10,20 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
-          <Script id="theme-init" strategy="afterInteractive">{`
-              const userTheme = localStorage.getItem('${KEY}')
-              const prefersDark = !userTheme && window.matchMedia('(prefers-color-scheme: dark)').matches
-              if (userTheme === '${Themes.dark}' || prefersDark) {
-                document.body.classList.add('${darkTheme}')
-              }
-            `}</Script>
         </Head>
 
         <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              const userTheme = localStorage.getItem('${KEY}')
+              const prefersDark = !userTheme && window.matchMedia('(prefers-color-scheme: dark)').matches
+              if (userTheme === '${Themes.dark}' || prefersDark) {
+                document.body.classList.add('${darkTheme.className}')
+              }
+            `,
+            }}
+          />
           <Main />
           <NextScript />
         </body>
